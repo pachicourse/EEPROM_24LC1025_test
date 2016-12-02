d3f9f0ee308be4b9b73dac6a8b29bc10f219cf4b
pachicourse <ddds78199500825@gmail.com>
2016-12-02 23:39:37 +0900

    Add files via upload

---
+++ EEPROM_24LC1025_test/EEPROM_24LC1025_test.ino
@@ -0,0 +1,53 @@
+#include <Wire.h>
+ 
+#define EPR_ADDR 0x51  // EEPROM address A2=Low,A1=Low,A0=HIGH
+ 
+void setup() {
+  SerialUSB.begin(9600);  // モニタ用にメッセージを出力
+  while(!SerialUSB){
+    ;
+  }
+  SerialUSB.println("EEPROM Test");
+ 
+  Wire.begin();       // I2C master
+  delay(1000);
+  
+  byte b[] = {5,10,20,40,80,160};
+  int len = sizeof(b) / sizeof(byte);
+
+//  eprWrite(0, b, len);
+  eprRead(0, len);
+}
+
+void loop() {
+}
+
+
+int eprWrite(int addr, byte b[], int l) {
+  int ans;
+
+  Wire.beginTransmission(EPR_ADDR);
+  Wire.write(highByte(addr));
+  Wire.write(lowByte(addr));
+  Wire.write(b, l);
+  ans = Wire.endTransmission();
+  
+  delay(5);
+  return ans;
+}
+ 
+void eprRead(int addr, int len) {
+  byte ret;
+  
+  Wire.beginTransmission(EPR_ADDR);
+  Wire.write(highByte(addr));
+  Wire.write(lowByte(addr));
+  Wire.endTransmission();
+  Wire.requestFrom(EPR_ADDR, len);
+ 
+  while (Wire.available()) {
+    ret = Wire.read();
+    SerialUSB.println(ret);
+  }
+  return;
+}
